2134b6cb5c0bfa4a0314d7e566a52e745ce5f89f
Potter <potteramylynn@gmail.com>
2023-06-06 13:55:22 -0500

    styled login, added header pictuer

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -7,8 +7,18 @@ export default function LoginPage() {
   return (
     <div className="App">
       <div className="LoginSignUp">
+        <div className="welcome">
+        <h2>Welcome,</h2>
+        <h4>please login or sign up to start growing in community!</h4>
+        </div>
+        <div className="component-container">
+          <div className="login col-6">
         <Login />
+        </div>
+        <div className="signup col-6">
         <Signup />
+        </div>
+        </div>
       </div>
     </div>
   );
