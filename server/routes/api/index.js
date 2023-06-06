2acb047b6d1df69f79b493c7de54da48f65c258f
Jhelt <jonhelton405@gmail.com>
2023-06-06 11:24:35 -0400

    fixed typo in index.js

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 
-router.use('/todo', postRoutes);
+router.use('/post', postRoutes);
 router.use('/user', userRoutes);
 
 module.exports = router;
