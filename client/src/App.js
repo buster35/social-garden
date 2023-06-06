0a17423a64df305ee1f1bbfe174934e91f3dae98
Potter <potteramylynn@gmail.com>
2023-06-06 11:18:31 -0500

    imported components for login, profile, and deleted wrapper in appjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Header, Wrapper } from "./components"
+import { Header, Footer} from "./components"
 import { UserProvider } from "./ctx/UserContext";
-import { HomePage, LoginPage, SignupPage, PostItem } from "./pages";
+import { HomePage, Login, PostItem } from "./pages";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/global.css'
@@ -12,17 +12,15 @@ function App() {
   return (
     <BrowserRouter>
       <UserProvider>
-        <Wrapper>
           <Header />
           <div className="pt-3 px-4">
             <Routes>
               <Route path="/" element={<HomePage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/login" element={<Login />} />
               <Route path="/post/:id" element={<PostItem />} />
             </Routes>
           </div>
-        </Wrapper>
+          <Footer />
       </UserProvider>
     </BrowserRouter>
   );
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,16 +1,17 @@
 // import { useState, useEffect } from "react"
 // import { Link } from "react-router-dom";
 // import { useUserContext } from "../ctx/UserContext"
-
+import { Weather, Chat, PhotoUpload} from "./../components"
 const HomePage = () => {
 
 
   return (
-    <>
+    <div className= 'App'>
       <h1>Home Page</h1>
-
-    
-    </>
+      < Weather />
+      < Chat />
+      < PhotoUpload />
+    </div >
   )
 }
 
