fce88bb85758a3a7a7adefd93feede75bcb26b5e
Potter <potteramylynn@gmail.com>
2023-06-08 13:08:39 -0500

    added to post function

--- client/src/components/WritePost.js
+++ client/src/components/WritePost.js
@@ -1,6 +1,7 @@
 import './writepost.css'
 import React from 'react';
 import {useState} from "react";
+import { useUserContext } from "../ctx/UserContext"
 
 const WritePost = () => {
   const currUser = useUserContext()
@@ -9,10 +10,34 @@ const WritePost = () => {
   
 
   const handleInputChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    e.preventDefault()
+    setPostText({ ...postText, [e.target.name]: e.target.value });
   };
-  console.log(formData)
+  console.log(postText, currUser)
+  
+  const handleFormSubmit = async (e) => {
+    e.preventDefault()
+    if (!currUser) return console.log("error");
+    const path= `api/post`
+    try {
+      const resp = await fetch(path, {
+        method: "post",
+        body: {
+          post: JSON.stringify(postText),
+          username: currUser.data.username,
+          currDate: Date.now,
+          userId: currUser.data._id
+        },
+        headers: {
+          "Content-Type": "application/json"
+        }
+    });
+    const result = await resp.jon()
+    console.log(result)
+    } catch(error) {
+      console.log("error")}
 
+}
 
   return (
      <div className="writePost">
@@ -20,7 +45,7 @@ const WritePost = () => {
       <label for="writePost">Write a Post</label>
         <input className= "postText" name= "postText" type="text" value= {formData.postText} onChange= {handleInputChange} rows={3}>
         </input>
-        <button className="postButton btn btn-secondary" onClick={handleFormSubmit}>Post</button>
+        <button className="postButton btn btn-secondary" type="submit">Post</button>
       </form>
      </div> 
     
