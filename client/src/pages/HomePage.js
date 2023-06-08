6508e4dfcc6e835fcc383eef34c34d750137a927
Anna Marlena <annamarlena310@gmail.com>
2023-06-08 11:39:13 -0500

    Starting to set up component placement

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,24 +2,27 @@
 // import { Link } from "react-router-dom";
 // import { useUserContext } from "../ctx/UserContext"
 import { Weather, Chat, PhotoUpload, Newsfeed, Login } from "./../components";
+import Container from 'react-bootstrap/Container';
 
 const HomePage = () => {
   return (
     <div className="App">
       <div className="App-home">
         <h3>Home Page</h3>
+          <Container>
+            <Row>
+              <Col lg={true}></Col>
+                <div className="Home-upload"><PhotoUpload /></div>
+                <div className="Home-newsfeed"><Newsfeed /></div>
+            </Row>
+          </Container>
         <div className="Home-weather container">
           <Weather />
         </div>
         <div className="Home-chat">
           <Chat />
         </div>
-        <div className="Home-upload">
-          <PhotoUpload />
-        </div>
-        <div className="Home-newsfeed">
-          <Newsfeed />
-        </div>
+
       </div>
     </div>
   );
