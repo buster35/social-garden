d67e70f676798b006a330d2392cb532d6b5cb21c
Anna Marlena <annamarlena310@gmail.com>
2023-06-08 12:27:29 -0500

    Trying to move the parts around

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,28 +1,24 @@
 // import { useState, useEffect } from "react"
 // import { Link } from "react-router-dom";
 // import { useUserContext } from "../ctx/UserContext"
-import { Weather, Chat, PhotoUpload, Newsfeed, Login } from "./../components";
-import Container from 'react-bootstrap/Container';
+import { Container } from "react-bootstrap";
+import { Weather, Chat, PhotoUpload, Newsfeed, WritePost, Login } from "./../components";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const HomePage = () => {
   return (
     <div className="App">
       <div className="App-home">
-        <h3>Home Page</h3>
-          <Container>
-            <Row>
-              <Col lg={true}></Col>
-                <div className="Home-upload"><PhotoUpload /></div>
-                <div className="Home-newsfeed"><Newsfeed /></div>
-            </Row>
-          </Container>
-        <div className="Home-weather container">
-          <Weather />
-        </div>
-        <div className="Home-chat">
-          <Chat />
-        </div>
-
+      <h3>Home Page</h3>
+        <Container>
+          <div className="write-post"><WritePost /></div>
+          <div className="Home-upload"><PhotoUpload /></div>
+          <div className="Home-newsfeed"><Newsfeed /></div>
+        </Container>
+        <Container>
+          <div className="Home-weather container"><Weather /></div>
+          <div className="Home-chat"><Chat /></div>
+        </Container>
       </div>
     </div>
   );
