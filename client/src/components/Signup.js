6554d7c8659260d97f915d3f360d25b58dff08eb
Jhelt <jonhelton405@gmail.com>
2023-06-09 16:05:25 -0400

    signup checkers

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -12,6 +12,28 @@ const SignupPage = (props) => {
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
+
+    // Log out lines 17 - 32 in order to bypass checks
+    // if (formData.password.length < 6 || formData.password.length > 20) {
+    //   setSignupResult("Password must be between 6 and 20 characters");
+    //   return;
+    // }
+
+    // const passwordRegex = /^(?=.*[A-Z])(?=.*\d).*$/;
+    // if (!passwordRegex.test(formData.password)) {
+    //   setSignupResult("Password must contain at least one capital letter and one number");
+    //   return;
+    // }
+
+
+    // const emailRegex = (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]);
+
+
+    // if (!emailRegex.test(formData.email)) {
+    //   setSignupResult("Invalid email address");
+    //   return;
+    // }
+
     const query = await fetch("/api/user", {
       method: "post",
       body: JSON.stringify(formData),
@@ -82,6 +104,25 @@ const SignupPage = (props) => {
           Signup failed!
         </div>
       )}
+
+      {signupResult === "Password must be between 6 and 20 characters" && (
+        <div className="alert alert-danger" role="alert">
+          Password must be between 6 and 20 characters!
+        </div>
+      )}
+
+      {signupResult === "Password must contain at least one capital letter and one number" && (
+        <div className="alert alert-danger" role="alert">
+          Password must contain at least one capital letter and one number
+        </div>
+      )}
+
+      {signupResult === "Invalid email address" && (
+        <div className="alert alert-danger" role="alert">
+          Invalid email address
+        </div>
+      )}
+
     </div>
   );
 };
