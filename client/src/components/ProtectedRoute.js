6ef3c88c6c3719b978e08ef3793cc088dedbef82
Jhelt <jonhelton405@gmail.com>
2023-06-09 14:41:47 -0400

    protected routes now working correctly!

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,17 +1,15 @@
-import { useNavigate } from "react-router-dom";
 import { useUserContext } from "../ctx/UserContext";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
   const { currUser } = useUserContext();
   console.log(currUser)
-  const navigate = useNavigate();
 
   if (currUser.status === "found" ) {
     console.log(currUser.status)
     return <Component {...rest} />;
   } else {
     console.log("You need to log in")
-    navigate("/login");
+    window.location.href = "/login"
   }
 };
 
