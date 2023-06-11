09e5565a01b22726088c2acb6975ea9e80e8c97f
Potter <potteramylynn@gmail.com>
2023-06-11 15:05:32 -0500

    websocket revert

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,18 +1,19 @@
-import "./chat.css";
-import React, { useState, useEffect, useRef } from "react";
-import { useUserContext } from "../ctx/UserContext";
-import Button from "react-bootstrap/Button";
+import "./chat.css"
+import React, { useState, useEffect, useRef} from "react";
+import { useUserContext } from "../ctx/UserContext"
+
 
 function ChatPage() {
   const [messages, setMessages] = useState([]);
   const [connection, setConnection] = useState(false);
   const [messageBody, setMessageBody] = useState();
 
-  const { currUser } = useUserContext();
+  const {currUser} = useUserContext()
 
-  console.log({ currUser });
+  console.log({currUser})
 
   const ws = useRef();
+
 
   const sendMessage = () => {
     if (messageBody) {
@@ -23,24 +24,24 @@ function ChatPage() {
         })
       );
       setMessageBody("");
-      console.log(messageBody);
+      console.log(messageBody)
     }
   };
 
   useEffect(() => {
-    // var host = location.origin.replace(/^http/, "ws");
-    // this.connection = new WebSocket(host);
-    // ws.current = new WebSocket(host);
     ws.current = new WebSocket("ws://localhost:8080");
+
 
     ws.current.onopen = () => {
       console.log("Chat connection opened");
       setConnection(true);
     };
 
+
     ws.current.onmessage = (e) => {
       const data = JSON.parse(e.data);
       setMessages((_messages) => [..._messages, data]);
+     
     };
 
     return () => {
@@ -58,50 +59,40 @@ function ChatPage() {
   }, [messages.length]);
 
   return (
-    <div>
-      <h5>Live Chat</h5>
     <div className="chat-container">
-      
-      <br></br>
+      <p>Live Chat:</p>
       <div className="chat-messages">
         {messages.map((message, index) => (
-          <div key={index}>
-            {message.sender} at
-            {new Date(message.sentAt).toLocaleTimeString(undefined, {
-              timeStyle: "short",
-            })}{" "}
-            {message.body}
+          <div key={index} >
+                    {message.sender} at 
+                      {new Date(message.sentAt).toLocaleTimeString(undefined, {
+                        timeStyle: "short",
+                      })}{" "}
+                  {message.body}
           </div>
         ))}
         <div ref={scrollTarget} />
       </div>
 
-      {/* <p>
+        {/* <p>
           You are chatting as {currUser.data.username}
         </p> */}
 
-      <div className="chat-input">
-        <input
-          id="message"
-          type="text"
-          name="message"
-          placeholder="Type your message here..."
-          value={messageBody}
-          onChange={(e) => setMessageBody(e.target.value)}
-          required
-        />
-        <br></br>
-        <br></br>
-        <Button
-          aria-label="Send"
-          type="send"
-          variant="light"
-          onClick={sendMessage}
-        >
-          Send
-        </Button>
-      </div>
-    </div>
+        <div className="chat-input">
+          <input
+            id="message"
+            type="text"
+            name= "message"
+            placeholder="Type your message here..."
+            value={messageBody}
+            onChange={(e) => setMessageBody(e.target.value)}
+            required
+          />
+          <button aria-label="Send" type="send" onClick={sendMessage} >
+            Send
+          </button>
+        </div>
+
     </div>
   );
 }
